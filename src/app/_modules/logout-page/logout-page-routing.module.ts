f6319bfb2c51a3814e6d06178f9dc832f8d1684e
Joshua Herrmann <joshua.herrmann1@web.de>
2022-09-30 00:18:29 +0200

    updated auth service

--- src/app/_modules/logout-page/logout-page-routing.module.ts
+++ src/app/_modules/logout-page/logout-page-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogoutPageComponent } from './logout-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'logout', component:LogoutPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/authentication/authentication.service.ts
+++ src/app/authentication/authentication.service.ts
@@ -1,13 +1,44 @@
+import { throwDialogContentAlreadyAttachedError } from '@angular/cdk/dialog';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router, RouterLink } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(public auth: AngularFireAuth) {
+  constructor(public auth: AngularFireAuth, public router: Router) {
    }
 
-   
+   loginWithEmailAndPassword(email:string, password:string){
+    this.auth.signInWithEmailAndPassword(email, password).then((data)=>{
+      console.log('Logged in', data);
+      this.router.navigateByUrl('/main');
+    }).catch((e)=>{
+      console.log('Error Log in', e);
+    });
+   }
+
+
+   logout(){
+    this.auth.signOut().then((data)=>{
+      console.log('Logged out', data);
+      this.router.navigateByUrl('/logout');
+    })
+    .catch((e)=>{
+      console.log('Error Logout', e);
+    });
+   }
+
+
+   signUpWithEmailAndPassword(email:string, password:string){
+    this.auth.createUserWithEmailAndPassword(email, password).then(data=>{
+      console.log('created new account', data);
+      this.router.navigateByUrl('/login');
+    })
+    .catch((e)=>{
+      console.log('error creating new account', e);
+    });
+   }
 }
